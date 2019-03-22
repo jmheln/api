fecdc8042d8d8853db8f3a780b3f6e3c79e105f9
jmheln <m3ng11@yahoo.com>
2019-03-22 10:33:07 +0800

    Passport Installed

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -22,6 +22,7 @@ class ProductResource extends JsonResource
             'discount' => $this->discount,
             'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2),
             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Rating yet',
+            
             'href' => [
                 'reviews' => route('review.index',$this->id)
             ]
