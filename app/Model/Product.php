ad68df7886abef03f4e7646676a3889bb9ded470
jmheln <m3ng11@yahoo.com>
2019-03-26 14:51:23 +0800

    Product Update

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -4,9 +4,14 @@ namespace App\Model;
 
 use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
+use Sumfony\Component\HttpFoundation\Response;
 
 class Product extends Model
 {
+
+    protected $fillable = [
+        'name','detail','stock','price','discount'
+    ];
     public function reviews()
 
     {
